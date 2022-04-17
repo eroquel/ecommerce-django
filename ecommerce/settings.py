a576ff880282da78cdbafdd483bc40144d2270cb
eroquel <e_roquel@hotmail.com>
2022-04-16 23:48:07 -0400

    Configuración - Deployment en Heroku 48

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from django.contrib.messages import constants as messages 
 MESSAGE_TAGS = {
