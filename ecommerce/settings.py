43d6bc360812b5b081e3ba9af266c8596b8f140f
eroquel <e_roquel@hotmail.com>
2022-04-17 00:00:55 -0400

    Configuración - Deployment en Heroku 52

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/' #Muy importante, en el tutorial dice que va **/media/** pero es **media/**
 MEDIA_ROOT = BASE_DIR /'media'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
